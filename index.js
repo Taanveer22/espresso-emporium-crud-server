ceab6c247dbca7c6a5af32784e1fb67073e45738
Taanveer22 <taanveer469@gmail.com>
2026-01-12 22:34:17 +0600

    alhamdulliah data received from client and send to database

--- index.js
+++ index.js
@@ -41,9 +41,18 @@ async function run() {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
 
+    const coffeesCollection = client.db("coffeesDB").collection("coffees");
+
     // ================= 05 server side routes==========================
     app.get("/", (req, res) => {
       res.send("server is working");
+    });
+
+    app.post("/coffees", async (req, res) => {
+      const newCoffee = req.body;
+      // console.log(newCoffee);
+      const result = await coffeesCollection.insertOne(newCoffee);
+      res.send(result);
     });
     // ==================================================================
     console.log(
