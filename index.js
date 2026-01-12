11de686862478275b7bfe750aa04e167c6e178f4
Taanveer22 <taanveer469@gmail.com>
2026-01-12 20:43:52 +0600

    alhamdulliah basic server setup done with mongo

---
+++ index.js
@@ -0,0 +1,61 @@
+// ==================Recommended Order=================
+// 1. Required by common js (express, cors, etc.)
+// 2 .Instance Initialization (const app = express())
+// 3. Middleware Setup (cors, json, logging)
+// 4. Database Configuration & Connection (MongoDB client setup and runMongoDB() function)
+// 5. Routes
+// 6. Server Startup (app.listen)
+// ===========================================================
+
+//=============== 01 required packages
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+
+//===================== 02 instance initialization
+const app = express();
+const port = process.env.PORT || 5000;
+
+//================= 03 use middleware
+app.use(cors());
+app.use(express.json());
+
+//======================= 04 mongodb config
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.89rnkti.mongodb.net/?appName=Cluster0`;
+console.log(uri);
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+
+    // ================= 05 server side routes==========================
+    app.get("/", (req, res) => {
+      res.send("server is working");
+    });
+    // ==================================================================
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } catch (error) {
+    console.log(error);
+  }
+}
+run();
+
+// ====================== 06 server startup
+app.listen(port, () => {
+  console.log(`server is running on port no. ${port}`);
+});
