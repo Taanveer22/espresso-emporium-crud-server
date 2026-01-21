04b5b52f8da699ec7a9ff6abc1f80df91957dc57
Taanveer22 <taanveer469@gmail.com>
2026-01-21 18:57:25 +0600

    alhamdulliah sign in time added in the sever and database

--- index.js
+++ index.js
@@ -111,6 +111,18 @@ async function run() {
       res.send(result);
     });
 
+    app.patch("/users", async (req, res) => {
+      const email = req.body.email;
+      const filter = { email };
+      const updateDoc = {
+        $set: {
+          lastSignInTime: req.body?.lastSignInTime,
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
     app.delete("/users/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
